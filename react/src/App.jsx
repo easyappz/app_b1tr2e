64e668da41703eb1d636c030943066cfecefc170
IbragimKhaniev <newinterny@gmail.com>
2025-11-15 06:41:59 +0300

    Fix critical routing issue: remove nested BrowserRouter from App.jsx; keep routing only in index.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Game from './components/Game';
 import './App.css';
@@ -14,12 +14,10 @@ function App() {
 
   return (
     <div className="app" data-easytag="id1-react/src/App.jsx">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/game" element={<Game />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
     </div>
   );
 }
